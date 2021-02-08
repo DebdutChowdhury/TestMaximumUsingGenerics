5da2ee911aced0a9b828080601adcfe16ac4d7fb
DebdutChowdhury <debdut.chowdhury2018@gmail.com>
2021-02-09 00:42:25 +0530

    REFACTOR: all the 3 to One Generic Method and find the maxium

--- src/main/java/com/generic/GenericMaximum.java
+++ src/main/java/com/generic/GenericMaximum.java
@@ -1,12 +1,17 @@
 package com.generic;
 
 public class GenericMaximum {
-    public String  number(String firstname, String secondname, String thirdname){
-        String max = firstname;
+    public <E extends Comparable> E number(E firstname, E secondname, E thirdname){
+        E max = firstname;
         if(secondname.compareTo(max) > 0)
             max = secondname;
         if(thirdname.compareTo(max) > 0)
             max = thirdname;
+        printName(max);
         return max;
+    }
+
+    public <E> void printName(E number){
+        System.out.println("Max name: "+number);
     }
 }
--- src/test/java/com/generic/GenericMaximumTest.java
+++ src/test/java/com/generic/GenericMaximumTest.java
@@ -5,20 +5,56 @@ import org.junit.Test;
 
 public class GenericMaximumTest {
     GenericMaximum maximum = new GenericMaximum();
+
     @Test
     public void givenfirstnum_shouldReturnMax() {
+        Integer max = maximum.number(3,2,1);
+        Assert.assertEquals((Integer)3, max);
+    }
+
+    @Test
+    public void givenSecondnum_shouldReturnMax() {
+        Integer max = maximum.number(2, 3, 1);
+        Assert.assertEquals((Integer)3, max);
+    }
+
+    @Test
+    public void givenThirdnum_shouldReturnMax() {
+        Integer max = maximum.number(1, 2, 3);
+        Assert.assertEquals((Integer)3, max);
+    }
+
+    public void givenFirstFloatNum_shouldReturnMax() {
+        Float max = maximum.number(3.5f,2.5f,1.5f);
+        Assert.assertEquals((Float) 3.5f, max);
+    }
+
+    @Test
+    public void givenSecondFloatnum_shouldReturnMax() {
+        Float max = maximum.number(2.5f, 3.5f, 1.5f);
+        Assert.assertEquals((Float) 3.5f, max);
+    }
+
+    @Test
+    public void givenThirdFloatnum_shouldReturnMax() {
+        Float max = maximum.number(1.5f, 2.5f, 3.5f);
+        Assert.assertEquals((Float) 3.5f, max);
+    }
+
+    @Test
+    public void givenfirstLargeName_shouldReturnLargename() {
         String max = maximum.number("peach", "apple", "mango");
         Assert.assertEquals((String) "peach", max);
     }
 
     @Test
-    public void givenSecondnum_shouldReturnMax() {
+    public void givenSecondLargeName_shouldReturnLargename() {
         String max = maximum.number("apple", "kiwi", "peach");
         Assert.assertEquals((String) "peach", max);
     }
 
     @Test
-    public void givenThirdnum_shouldReturnMax() {
+    public void givenThirdLargeName_shouldReturnLargename() {
         String max = maximum.number("apple", "kiwi", "peach");
         Assert.assertEquals((String) "peach", max);
     }
